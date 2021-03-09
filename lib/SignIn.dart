68f92adc43470607f4cdd8e52c38630b8a3d4652
dangvuminh <dangvuminh98@gmail.com>
2021-03-09 09:13:04 +0700

    create my own appbars

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -12,145 +12,178 @@ class SignIn extends StatelessWidget {
             Navigator.pushNamed(context, '/signInList');
           },
         ),
-      appBar: AppBar(
-        title: Text('Sign In'),
-        backgroundColor: Colors.grey[850],
-      ),
+      // appBar: AppBar(
+      //   title: Text('Sign In'),
+      //   backgroundColor: Colors.grey[850],
+      // ),
       body: Container(
         color: Colors.black,
-        child: Column(
-          children: [
-            Container(
-              height: 250.0,
-            ),
-            Expanded(child: Container(
-              child: Column(
-                children: [
-                  Container(
-                    width: 500.0,
-                    padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                    child: Text(
-                      'Email or username',
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.grey[400],
-                        fontWeight: FontWeight.w500,
-                      )
+        child: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(top:10.0,bottom:10.0),
+                color: Colors.grey[850],
+                child: Row(
+                  children: [
+                    FlatButton(
+                      onPressed: (){
+                        Navigator.pop(context,'/');
+                      },
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.0,
+                          )
+                        ),
                     ),
-                  ),
-                  Container(
-                    width: 400.0,
-                    height: 50.0,
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        fillColor: Colors.grey[600],
-                        filled: true,
-                      ),
+                    Container(
+                        margin:EdgeInsets.only(left: 80.0) ,
+                        child: Text(
+                            'Sign In',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                            )
+                        )
                     ),
-                  ),
+                  ],
+                ),
+              ),
 
-                  Container(
-                    width: 500.0,
-                    margin: EdgeInsets.only(top:15.0),
-                    padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                    child: Text(
-                        'Password',
+              Container(
+                height: 250.0,
+              ),
+              Expanded(child: Container(
+                child: Column(
+                  children: [
+                    Container(
+                      width: 500.0,
+                      padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      child: Text(
+                        'Email or username',
                         textAlign: TextAlign.start,
                         style: TextStyle(
                           color: Colors.grey[400],
                           fontWeight: FontWeight.w500,
                         )
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: 400.0,
-                    height: 50.0,
-                    child: Stack(
-                      children: [
-                         TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            fillColor: Colors.grey[600],
-                            filled: true,
-                          ),
+                    Container(
+                      width: 400.0,
+                      height: 50.0,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          fillColor: Colors.grey[600],
+                          filled: true,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Icon(
-                            Icons.remove_red_eye_outlined,
-                            size: 30.0,
-                          ),
-                        )
-                      ],
+                      ),
                     ),
-                  ),
 
-                  Container(
-                    margin: EdgeInsets.only(top:15.0),
-                    padding: EdgeInsets.fromLTRB(170, 15, 170, 15),
-                    child: Text(
-                        'Sign In',
-                        style: TextStyle(
-                          decoration: TextDecoration.none,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                          fontSize: 18.0,
-                        )
+                    Container(
+                      width: 500.0,
+                      margin: EdgeInsets.only(top:15.0),
+                      padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                      child: Text(
+                          'Password',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            color: Colors.grey[400],
+                            fontWeight: FontWeight.w500,
+                          )
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      color: Colors.lightBlue,
+                    Container(
+                      width: 400.0,
+                      height: 50.0,
+                      child: Stack(
+                        children: [
+                           TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              fillColor: Colors.grey[600],
+                              filled: true,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Icon(
+                              Icons.remove_red_eye_outlined,
+                              size: 30.0,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 20.0),
-                  Text(
-                      'Need help ?',
-                      style: TextStyle(
+
+                    Container(
+                      margin: EdgeInsets.only(top:15.0),
+                      padding: EdgeInsets.fromLTRB(170, 15, 170, 15),
+                      child: Text(
+                          'Sign In',
+                          style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          )
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
                         color: Colors.lightBlue,
-                      )
-                  ),
-                  SizedBox(height: 20.0),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
-                    child: Text(
-                        'Use Single Sign-On (SSO)',
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text(
+                        'Need help ?',
                         style: TextStyle(
-                          decoration: TextDecoration.none,
-                          fontWeight: FontWeight.w600,
                           color: Colors.lightBlue,
-                          fontSize: 18.0,
                         )
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.lightBlue),
-                      borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                    SizedBox(height: 20.0),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(80, 10, 80, 10),
+                      child: Text(
+                          'Use Single Sign-On (SSO)',
+                          style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.lightBlue,
+                            fontSize: 18.0,
+                          )
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.lightBlue),
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 15.0),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(90, 10, 90, 10),
-                    child: Text(
-                        'Subscribe to PluralSight',
-                        style: TextStyle(
-                          decoration: TextDecoration.none,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.lightBlue,
-                          fontSize: 18.0,
-                        )
+                    SizedBox(height: 15.0),
+                    Container(
+                      padding: EdgeInsets.fromLTRB(90, 10, 90, 10),
+                      child: Text(
+                          'Subscribe to PluralSight',
+                          style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.lightBlue,
+                            fontSize: 18.0,
+                          )
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.lightBlue),
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.lightBlue),
-                      borderRadius: BorderRadius.all(Radius.circular(3.0)),
-                    ),
-                  ),
-                  SizedBox(height: 15.0),
-                ],
-              ),
-            )),
-          ],
+                    SizedBox(height: 15.0),
+                  ],
+                ),
+              )),
+            ],
+          ),
         ),
       )
     );
--- lib/SignInList.dart
+++ lib/SignInList.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_flutter/SignIn.dart';
 
 class SignInList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.grey,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-                'Sign In',
-            ),
-          ],
-        ),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.grey,
+      //   title: Row(
+      //     mainAxisAlignment: MainAxisAlignment.start,
+      //     children: [
+      //       Text(
+      //           'Sign In',
+      //       ),
+      //     ],
+      //   ),
+      // ),
       backgroundColor: Colors.black,
       floatingActionButton: FloatingActionButton(
         child: Icon(
@@ -24,116 +25,144 @@ class SignInList extends StatelessWidget {
           Navigator.pushNamed(context, '');
         },
       ),
-      body: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Text(
-                      'Forgot your password?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                  Expanded(child: Container()),
-                  Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.white,
-                  )
-                ],
-              ),
-            ),
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top:10.0,bottom:10.0),
+              color: Colors.grey[850],
+              child: FlatButton(
+                
+                child:Row(
+                  children: [
+                    Icon(
+                          Icons.arrow_back_ios,
+                          color: Colors.white,
+                      ),
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color:Colors.white,
+                        fontSize: 22.0,
+                      ),
+                    )
+                  ],
+                ),
+                onPressed: (){
+                  Navigator.pop(context, '/signIn');
+                },
+              )
 
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
-              ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Subscribe to PluralSight',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Text(
+                        'Forgot your password?',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
                     ),
-                  ),
-                  Expanded(child: Container()),
-                  Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.white,
-                  )
-                ],
+                    Expanded(child: Container()),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
 
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+                ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Restore subscription with Itunes',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Subscribe to PluralSight',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
                     ),
-                  ),
-                  Expanded(child: Container()),
-                  Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.white,
-                  )
-                ],
+                    Expanded(child: Container()),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
 
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+                ),
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                children: [
-                  Text(
-                    'Contact PluralSight support',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Restore subscription with Itunes',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
                     ),
-                  ),
-                  Expanded(child: Container()),
-                  Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.white,
-                  )
-                ],
+                    Expanded(child: Container()),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
               ),
             ),
-          )
-        ],
+
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey, width: 0.5, style: BorderStyle.solid),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Contact PluralSight support',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    Expanded(child: Container()),
+                    Icon(
+                      Icons.arrow_forward_ios_rounded,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
